435ef5aba107956e66c01475d057985d4f3792fc
Justin Lamb <justinlamb@me.com>
2022-03-24 11:48:03 -0700

    save and load game added

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,4 +1,5 @@
 require './lib/hangmanpics.rb'
+require 'erb'
 
 class WORD
     attr_accessor :file
@@ -56,7 +57,7 @@ class GUESS
         @game_word = game_word
     end
 
-    def input_validation(guess, letters_guessed)
+    def input_validation(guess='no guess', letters_guessed)
         @letters_guessed = letters_guessed
         @guess = guess
  
@@ -82,16 +83,52 @@ class GUESS
     end
 end
 
+class SAVE
+    attr_accessor :erb
+    def initialize
+        @template = File.read('save_state.erb')
+        @erb = ERB.new @template
+    end
+
+    def save_game(game_word, incorrect_guess_num, letters_guessed, word_progress)
+        Dir.mkdir('saves') unless Dir.exist?('saves')
+
+        filename = "saves/save_data.rb"
+        save_data = erb.result(binding)
+
+        File.open(filename, 'w') do |file|
+            file.puts save_data
+        end
+
+    end
+
+    def load_game
+        if File.exist?('./saves/save_data.rb')
+            require './saves/save_data.rb'
+            return Save_state::GAME_WORD, Save_state::INCORRECT_GUESS_NUM, Save_state::LETTERS_GUESSED, Save_state::WORD_PROGRESS
+        else
+            puts 'No saves found.'
+        end
+
+
+    end
+end
+
 class GAME
     puts "Welcome to Hangman"
-    game_word = WORD.new.secret_word().chomp
-
-    incorrect_guess_num = 0
-    letters_guessed = []
-    word_progress = Array.new(game_word.length, "_ ")
+    puts "Would you like to load a saved game? 'y' or 'n'"
+    open_load = gets.chomp.downcase
+    if open_load == 'y'
+        game_word, incorrect_guess_num, letters_guessed, word_progress = SAVE.new.load_game
+    else
+        game_word = WORD.new.secret_word().chomp
+        incorrect_guess_num = 0
+        letters_guessed = []
+        word_progress = Array.new(game_word.length, "_ ")
+    end
 
     gameboard = DRAW.new(game_word)
-    gameboard.update_hangman()
+    gameboard.update_hangman(incorrect_guess_num, '', [], word_progress)
 
     while incorrect_guess_num < Pics::HANGMANPICS.length - 1 # Continue to ask for guess until full hangman has been drawn
         puts "Guess a letter"
@@ -116,8 +153,15 @@ class GAME
             puts "\nYou win!\n\n"
             break
         end 
+        puts "Save game and quit? Enter 'y' to save or any key to continue."
+        save = gets.chomp.downcase
+        if save == 'y'
+            SAVE.new.save_game(game_word, incorrect_guess_num, letters_guessed, word_progress)
+            puts "Game has been saved. Thanks for playing!"
+            break
+        end
     end
-    unless game_word == word_progress.join()
+    unless game_word == word_progress.join() || save == 'y'
         puts "\nYou ran out of guesses! The correct word was: #{game_word}\n\n"
     end
 end
---
+++ saves/save_data.rb
@@ -0,0 +1,7 @@
+
+module Save_state
+    GAME_WORD = 'blocks'
+    INCORRECT_GUESS_NUM = 1
+    LETTERS_GUESSED = ["l", "k", "i"]
+    WORD_PROGRESS = ["_ ", "l", "_ ", "_ ", "k", "_ "]
+end
